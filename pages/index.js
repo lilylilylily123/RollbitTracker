bd2d04ebede948d0f95b4be94c20029aef0150ca
Lily G <aksg656@icloud.com>
2023-04-04 01:59:41 +0200

    maybe fix!:!.

--- pages/index.js
+++ pages/index.js
@@ -4,8 +4,6 @@ import Searchbar from "@/components/Searchbar/Searchbar";
 import Affiliation from "@/components/OTHER/Affiliation/Affiliation";
 import Image from "next/image";
 import Nav from "@/components/Navbar/Nav";
-import 'react-tooltip/dist/react-tooltip.css';
-import {Tooltip} from 'react-tooltip';
 
 
 
@@ -27,15 +25,13 @@ export default function Home() {
           <div className={styles.search}>
             <Searchbar />
           </div>
-            <div data-tooltip-id={"tooltip"} data-tooltip-html={""} data-html={true} className={styles.promocode}>
+
+            <div className={styles.promocode}>
                 <h1>Code: <span>ROLLTRACK</span> & /tip mmikebike09</h1>
             </div>
-            <Tooltip id={"tooltip"}
-                     style={{backgroundColor: "#1F202A", color: "white", border: "none", borderRadius: "0.9vw", padding: "20px", fontSize: "1.2rem", zIndex: 1}}
-            />
-          <div className={styles.affiliation}>
+            <div className={styles.affiliation}>
               <Affiliation />
-          </div>
+            </div>
             <div className={styles.navigation}>
                 <Nav chart={true} track={true} color={"#3B3E50"}/>
             </div>
